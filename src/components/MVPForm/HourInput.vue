c7fbb1da61f6301609e11744ec3a1c08bc849398
Alison <alisonserafim@gmail.com>
2022-07-18 22:59:37 -0300

    feat: add mask in hour input

--- src/components/MVPForm/HourInput.vue
+++ src/components/MVPForm/HourInput.vue
@@ -5,11 +5,24 @@
     name="hour"
     id="hour"
     placeholder="Type Hour"
+    @keyup="maskHour($event)"
   />
 </template>
 <script>
 export default {
-    name: 'HourInput'
+    name: 'HourInput',
+    methods: {
+      maskHour: function(event) {
+         if (event.target.value.length == 2) {
+           if (event.keyCode == 8) {
+            return
+           }
+           else {
+            event.target.value += ":";
+           }
+        }
+      }
+    }
 };
 </script>
 <style></style>
