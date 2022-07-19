989a9c5e07422e6a00d087aa63a639f6ffe65846
Alison <alisonserafim@gmail.com>
2022-07-18 22:58:50 -0300

    feat: add mvp list in form input

--- src/components/MVPForm/MVPListSelect.vue
+++ src/components/MVPForm/MVPListSelect.vue
@@ -1,9 +1,27 @@
 <template>
-  <select name="mvp-list" id="mvp-list" class="form-control col-md-12"></select>
+  <select
+    name="mvp-list"
+    id="mvp-list"
+    class="form-control col-md-12"
+    @change="changeMVP($event)"
+  >
+    <option value="0">-</option>
+    <option v-for="(mvp, index) in mvp_list" :value="mvp.id" :key="index">
+      {{ mvp.name }}
+    </option>
+  </select>
 </template>
 <script>
+import { mapState, mapMutations } from "vuex";
 export default {
-  name: 'MVPListSelect'
+  name: "MVPListSelect",
+  computed: mapState(["mvp_list"]),
+  methods: {
+    ...mapMutations(["setCurrentMvpId"]),
+    changeMVP: function (event) {
+      this.setCurrentMvpId(event.target.value);
+    },
+  },
 };
 </script>
 <style></style>
